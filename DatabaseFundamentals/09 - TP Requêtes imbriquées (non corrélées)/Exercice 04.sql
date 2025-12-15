14901662eebae2d49b8454f1859dbaef76047cc9
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2025-12-15 20:22:53 +0100

    Update Exercice 04.sql

--- DatabaseFundamentals/09 - TP Requêtes imbriquées (non corrélées)/Exercice 04.sql
+++ DatabaseFundamentals/09 - TP Requêtes imbriquées (non corrélées)/Exercice 04.sql
@@ -1,3 +1,18 @@
+-- Première manière : deux IN
+
+SET GLOBAL SQL_MODE = CONCAT(@@SQL_MODE, ',ONLY_FULL_GROUP_BY');
+
+SELECT s.SNAME
+FROM S s
+WHERE s.ID_S IN (SELECT li.ID_S
+                 FROM SPJ li
+                 WHERE li.ID_P IN (SELECT p.ID_P
+                                   FROM P p
+                                   WHERE p.COLOR = 'Red'));
+
+
+-- Seconde manière : un IN et une jointure
+
 SET GLOBAL SQL_MODE = CONCAT(@@SQL_MODE, ',ONLY_FULL_GROUP_BY');
 
 SELECT DISTINCT s.SNAME
