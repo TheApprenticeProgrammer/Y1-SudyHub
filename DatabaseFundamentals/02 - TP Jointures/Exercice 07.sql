811eaa0f0bab478843553e2d55dfff2ed5d09fc5
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2025-12-30 16:51:08 +0100

    Create Exercice 07.sql

---
+++ DatabaseFundamentals/02 - TP Jointures/Exercice 07.sql
@@ -0,0 +1,5 @@
+SELECT s.SNAME AS NOM, p.PNAME AS PIECE, j.JNAME AS PROJET, li.QTY AS QUANTITE
+FROM SPJ li JOIN S s ON li.ID_S = s.ID_S
+            JOIN P p ON li.ID_p = p.ID_P
+            JOIN J j ON li.ID_J = j.ID_J
+WHERE s.CITY = 'Paris';
