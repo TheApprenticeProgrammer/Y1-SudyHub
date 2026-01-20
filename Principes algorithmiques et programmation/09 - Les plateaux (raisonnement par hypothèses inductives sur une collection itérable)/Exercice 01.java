86ccebb6c58dfe9430602369de3b55d5f05e7906
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-01-20 17:00:25 +0100

    Update Exercice 01.java

--- Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 01.java
+++ Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 01.java
@@ -1,26 +1,35 @@
 import seqint.SeqInt;
 import seqint.SeqIntIterator;
 
+/*
+
+res = 0
+
+prec        1 1 8 8 0 6 5 5 / prec = e
+e         1 1 8 8 0 6 5 5 5 / if (it.hasNext())
+res     0 1 1 2 2 3 4 5 5 5 / if (e != prec) ++res
+
+ */
+
 public class Main {
 
     public static int nombrePlateaux(SeqInt s) {
 
         SeqIntIterator it = s.iterator();
 
-        int nbPlat = 0;
-
+        int res = 0;
         if (it.hasNext()) {
             int e = it.next();
-            nbPlat = 1;
+            res = 1;
             while (it.hasNext()) {
                 int prec = e;
                 e = it.next();
                 if (e != prec) {
-                    ++nbPlat;
+                    ++res;
                 }
             }
         }
-        return nbPlat;
+        return res;
     }
 
     public static void main(String[] args) {
@@ -30,8 +39,7 @@ public class Main {
         System.out.println("0 = " + nombrePlateaux(s1));
         SeqInt s2 = new SeqInt(1);
         System.out.println("1 = " + nombrePlateaux(s2));
-        SeqInt s3 = new SeqInt(-1 -1, 8, 8, 0, 6, 5, 5, 5);
+        SeqInt s3 = new SeqInt(1, 1, 8, 8, 0, 6, 5, 5, 5);
         System.out.println("5 = " + nombrePlateaux(s3));
     }
 }
-
