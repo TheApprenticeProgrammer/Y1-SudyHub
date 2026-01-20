0560dfcdc1af72a5a762cee8b0fcb4295ec0a146
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-01-20 21:54:06 +0100

    Update Exercice 01.java

--- Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 01.java
+++ Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 01.java
@@ -2,44 +2,48 @@ import seqint.SeqInt;
 import seqint.SeqIntIterator;
 
 /*
+Écrire une fonction qui calcule le nombre de plateaux d’une séquence.
 
-res = 0
+e = élément courant
+prec = élément précédent
+nbPlateaux = nombre de plateaux rencontrès
 
-prec        1 1 8 8 0 6 5 5 / prec = e
-e         1 1 8 8 0 6 5 5 5 / if (it.hasNext())
-res     0 1 1 2 2 3 4 5 5 5 / if (e != prec) ++res
-
- */
+prec             5 2 1 1 1 2 2 7 7 7 7 1    =>  prec = e
+-----------------------------------------
+e               5 2 1 1 1 2 2 7 7 7 7 1 2   => e = it.next();
+-----------------------------------------
+nbPlateaux      5 2 1 1 1 2 2 7 7 7 7 1 2   => if (e != prec) ++nbPlateaux;
+*/
 
 public class Main {
 
     public static int nombrePlateaux(SeqInt s) {
-
         SeqIntIterator it = s.iterator();
-
-        int res = 0;
+        int nbPlateaux = 0;
         if (it.hasNext()) {
             int e = it.next();
-            res = 1;
+            nbPlateaux = 1;
             while (it.hasNext()) {
                 int prec = e;
                 e = it.next();
                 if (e != prec) {
-                    ++res;
+                    ++nbPlateaux;
                 }
             }
         }
-        return res;
+        return nbPlateaux;
     }
 
     public static void main(String[] args) {
+        SeqInt sVide = new SeqInt();
+        System.out.println(sVide);
+        System.out.println("nbPlateaux : 0 = " + nombrePlateaux(sVide));
+        System.out.println();
 
-        System.out.println("Zone de test : ");
-        SeqInt s1 = new SeqInt();
-        System.out.println("0 = " + nombrePlateaux(s1));
-        SeqInt s2 = new SeqInt(1);
-        System.out.println("1 = " + nombrePlateaux(s2));
-        SeqInt s3 = new SeqInt(1, 1, 8, 8, 0, 6, 5, 5, 5);
-        System.out.println("5 = " + nombrePlateaux(s3));
+        SeqInt s = new SeqInt(5, 2, 1, 1, 1, 2, 2, 7, 7, 7, 7, 1, 2);
+        System.out.println(s);
+        System.out.println("nbPlateaux : 7 = " + nombrePlateaux(s));
+        System.out.println();
     }
 }
+
