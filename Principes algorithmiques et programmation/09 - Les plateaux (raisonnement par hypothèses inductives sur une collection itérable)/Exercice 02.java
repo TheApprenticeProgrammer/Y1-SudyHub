073d7e74463037f5cd60353b62af375e5612cd11
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-01-20 22:23:37 +0100

    Update Exercice 02.java

--- Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 02.java
+++ Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 02.java
@@ -1,47 +1,52 @@
-/*
-Écrire une fonction qui calcule le nombre de nombre de blocs de même parité d’une séquence.
- */
 import seqint.SeqInt;
 import seqint.SeqIntIterator;
 
 /*
+Écrire une fonction qui calcule le nombre de nombres
+de blocs de même parité d’une séquence.
 
-res = 0
+prec  = élément précédent
+e = élément courant
+nbBlocs = nombre de blocs de même parité
 
-prec      1 1 2 2 2 5 5 / prec = e
-e       1 1 2 2 2 5 5 8 / if (it.hasNext()) e = it.next()
-res     1 1 2 2 2 3 3 4 / if (e % 2 != prec % 2) ++res
+prec          5 5 1 1 1 2 2 4 8 2 2 1 2     =>  prec = e
+---------------------------------------
+e           5 5 1 1 1 2 2 4 8 2 2 1 2 2     => e = it.next();
+---------------------------------------
+nbBlocs     1 1 1 1 1 2 2 2 2 2 2 3 4 4     => if (e % 2 != prec % 2) ++nbBlocs
 
+Pré-initialisation :
+    int nbBlocs  = 0
  */
 
 public class Main {
 
-    public static int nombreBlocsparité(SeqInt s) {
-
+    public static int nombreBlocs(SeqInt s) {
         SeqIntIterator it = s.iterator();
-
-        int res = 0;
-
+        int nbBlocs = 0;
         if (it.hasNext()) {
             int e = it.next();
-            res = 1;
+            nbBlocs = 1;
             while (it.hasNext()) {
                 int prec = e;
                 e = it.next();
                 if (e % 2 != prec % 2) {
-                    ++res;
+                    ++nbBlocs;
                 }
             }
         }
-        return res;
+        return nbBlocs;
     }
 
     public static void main(String[] args) {
-        SeqInt s1 = new SeqInt();
-        System.out.println("0 = " + nombreBlocsparité(s1));
-        SeqInt s2 = new SeqInt(1, 1);
-        System.out.println("1 = " + nombreBlocsparité(s2));
-        SeqInt s3 = new SeqInt(1, 1, 2, 2, 2, 5, 5, 8);
-        System.out.println("4 = " + nombreBlocsparité(s3));
+        SeqInt sVide = new SeqInt();
+        System.out.println(sVide);
+        System.out.println("nbBlocs : 0 = " + nombreBlocs(sVide));
+        System.out.println();
+
+        SeqInt s = new SeqInt(5, 5, 1, 1, 1, 2, 2, 4, 8, 2, 2, 1, 2);
+        System.out.println(s);
+        System.out.println("nbBlocs : 4 = " + nombreBlocs(s));
+        System.out.println();
     }
 }
