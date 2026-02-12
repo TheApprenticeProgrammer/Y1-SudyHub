4dfe6f14f38f40c7ecb86a1677ef6d0211e7b23b
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-02-12 21:16:49 +0100

    Update Exercice 01.java

--- Principes algorithmiques et programmation/08 - Les séquences/Exercice 01.java
+++ Principes algorithmiques et programmation/08 - Les séquences/Exercice 01.java
@@ -4,11 +4,11 @@ import seqint.SeqIntIterator;
 /*
 Écrivez une fonction booléenne qui détermine si une séquence est vide.
 
-Version du professeur :
+Version du corrigé :
 
     public static boolean estVide(SeqInt s) {
         SeqIntIterator it = s.iterator();
-        return !it.hasNext();
+        return !it.hasNext(); // it.hasNext() renvoie false si il n'y a plus de valeur après la position de l'itérateur
     }
 */
 
@@ -16,27 +16,17 @@ public class Main {
 
     public static boolean estVide(SeqInt s) {
         SeqIntIterator it = s.iterator();
-        boolean estVide = true;
+        boolean vide = true;
         if (it.hasNext()) {
-           estVide = false;
+            vide = false;
         }
-        return estVide;
+        return vide;
     }
 
     public static void main(String[] args) {
         SeqInt s1 = new SeqInt();
-        if (estVide(s1)) {
-            System.out.println("Cette séquence est vide.");
-        }
-        else {
-            System.out.println("Cette séquence n'est pas vide.");
-        }
         SeqInt s2 = new SeqInt(1, 2, 3, 4, 5);
-        if (estVide(s2)) {
-            System.out.println("Cette séquence est vide.");
-        }
-        else {
-            System.out.println("Cette séquence n'est pas vide.");
-        }
+        System.out.println("s1 est vide : " + estVide(s1));
+        System.out.println("s2 est vide : " + estVide(s2));
     }
 }
