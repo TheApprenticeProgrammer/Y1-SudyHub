2e798396d332b34ccd4aac81ca97cf70354bfc3d
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-02-16 21:29:20 +0100

    Update Exercice 03.java

--- Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 03.java
+++ Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 03.java
@@ -5,43 +5,49 @@ import seqint.SeqIntIterator;
 Écrire une fonction qui calcule le nombre de plateaux qui précèdent la première occurrence du
 maximum.
 
-prec = élément précédent
 e = élément courant
-res = réponse
-nbPlateaux = nombre de plateaux rencontrés
+prec = élément précédent
+nbPlat = nombre de plateaux rencontrés jusqu'ici indépendamment du max
+res = nombre de plateaux avant la première occurrence du max
 
-prec              5 2 1 1 1 2 2 7 7 7 7 1   =>  prec = e
------------------------------------------
-e               5 2 1 1 1 2 2 7 7 7 7 1 2   =>  if(it.hasNext()) e = it.next()
------------------------------------------
-max             5 5 5 5 5 5 5 7 7 7 7 7 7   =>  if (e > max) max = e
------------------------------------------
-res             0 0 0 0 0 0 0 4 4 4 4 4 4   =>  if (e > max) res = nbPlateaux
------------------------------------------
-nbPlateaux      1 2 3 3 3 4 4 5 5 5 5 6 7   =>  if (e != prec) ++nbPlateaux
+prec              1 1 5 5 5 4 4 3 3 3 5 5 5     =>  prec = e
+e               1 1 5 5 5 4 4 3 3 3 5 5 5 7     =>  e = it.next()
+max             1 1 5 5 5 5 5 5 5 5 5 5 5 7     =>  if (e > max) max = e
+nbPlat          1 1 2 2 2 3 3 4 4 4 5 5 5 6     =>  if (e != prec) ++nbPlat
+res             0 0 1 1 1 1 1 1 1 1 1 1 1 5     =>  if (e > max) res = nbPlat
 
-Pré-initialisation :
-    int res = 0
+Pré-Initialisation :
+    int res = 0  => retourne zéro si la séquence est vide ou si il n'y a pas de plateau qui précèdent la première
+                    occurence du max
+
+Initialisation :
+    if (it.hasNext())
+        int e = it.next()
+        int max = e
+        nbPlat = 1
+        while (it.hasNext())
+            int prec = e
+            e = it.next() ...
 */
 
 public class Main {
 
-    public static int nombrePlatAvantMax(SeqInt s) {
+    public static int nbPlatBeforeMax(SeqInt s) {
         SeqIntIterator it = s.iterator();
         int res = 0;
         if (it.hasNext()) {
             int e = it.next();
             int max = e;
-            int nbPlateaux = 1;
+            int nbPlat = 1;
             while (it.hasNext()) {
                 int prec = e;
                 e = it.next();
                 if (e != prec) {
                     if (e > max) {
                         max = e;
-                        res = nbPlateaux;
+                        res = nbPlat;
                     }
-                    ++nbPlateaux;
+                    ++nbPlat;
                 }
             }
         }
@@ -49,14 +55,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        SeqInt sVide = new SeqInt();
-        System.out.println(sVide);
-        System.out.println("nbPlateauxAvantMax : 0 = " + nombrePlatAvantMax(sVide));
-        System.out.println();
-
-        SeqInt s = new SeqInt(5, 2, 1, 1, 1, 2, 2, 7, 7, 7, 7, 1, 2);
-        System.out.println(s);
-        System.out.println("nbPlateauxAvantMax : 4 = " + nombrePlatAvantMax(s));
-        System.out.println();
+        SeqInt s1 = new SeqInt();
+        SeqInt s2 = new SeqInt(1, 1, 5, 5, 5, 4, 4, 3, 3, 3, 5, 5, 5, 7);
+        SeqInt s3 = new SeqInt(5, 1, 2, 2, 3, 3, 4, 4, 5, 5);
+        System.out.println("0 = " + nbPlatBeforeMax(s1));
+        System.out.println("5 = " + nbPlatBeforeMax(s2));
+        System.out.println("0 = " + nbPlatBeforeMax(s3));
     }
 }
