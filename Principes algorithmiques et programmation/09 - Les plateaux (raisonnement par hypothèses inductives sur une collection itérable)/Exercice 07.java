cf229a203b9d69930d87401215fbbf3f9ac51cc2
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-02-18 12:17:25 +0100

    Update Exercice 07.java

--- Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 07.java
+++ Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 07.java
@@ -8,25 +8,29 @@ import seqint.SeqIntIterator;
 e = élément courant
 prec = élément précédent
 lgBloc = longueur du bloc de même parité courant
-LG = paramètre fourni à la fonction
-res = nombre de blocs de longueur inférieure ou égale à LG
+LG = paramètre envoyé à la fonction
+res = nombre de blocs de même parité de longueur inférieure ou égale à LG
 
-prec              2 4 1 3 1 8 6 8 8 2 4 1 7 8 8 8 1     =>  prec = e
-e               2 4 1 3 1 8 6 8 8 2 4 1 7 8 8 8 1 2     =>  if (it.hasNext()) e = it.next()
-lgBloc          1 2 1 2 3 1 2 3 4 5 6 1 2 1 2 3 1 1     =>  if (e % 2 == prec % 2) ++lgBloc else lgBloc = 1
-LG[03]          1 1 2 2 2 3 3 3 2 2 2 3 3 4 4 4 5 6     =>  if (lgBloc == 1) ++res else if (lgBloc == (LG + 1)) --res
-LG[02]          1 1 2 2 1 2 2 1 1 1 1 2 2 3 3 2 3 4     =>  if (lgBloc == 1) ++res else if (lgBloc == (LG + 1)) --res
-LG[01]          1 0 1 0 0 1 0 0 0 0 0 1 0 1 0 0 1 2     =>  if (lgBloc == 1) ++res else if (lgBloc == (LG + 1)) --res
+prec              1 1 3 4 4 8 6 7 7 1 4 1 4 1 1 8 6     => prec = e
+e               1 1 3 4 4 8 6 7 7 1 4 1 4 1 1 8 6 6     => e = it.next()
+lgBloc          1 2 3 1 2 3 4 1 2 3 1 1 1 1 2 1 2 3     => if (e % 2 != prec % 2) lgBloc = 1 else ++lgBloc
+LG[01]          1 0 0 1 0 0 0 1 0 0 1 2 3 4 3 4 3 3     => if (lgBloc == 1) ++res else if (lgBloc == LG + 1) --res
+LG[02]          1 1 0 1 1 0 0 1 1 0 1 2 3 4 4 5 5 4     => if (lgBloc == 1) ++res else if (lgBloc == LG + 1) --res
+LG[03]          1 1 1 2 2 2 1 2 2 2 3 4 5 6 6 7 7 7     => if (lgBloc == 1) ++res else if (lgBloc == LG + 1) --res
 
 Pré-Initialisation :
-    int res = 0
+    int res = 0   => retourne zéro si la séquence est vide ou si il n'y a pas de blocs de même parité de longueur
+                     inférieure ou égale à LG
 
-Inititialisation :
-    int e = it.next()
-    int lgBloc = 1
-    res = 1
- */
-
+Initialisation :
+    if (it.hasNext())
+        int e = it.next()
+        int lgBloc = 1
+        res = 1
+        while (it.hasNext())
+            int prec = e
+            e = it.next() ...
+*/
 
 public class Main {
 
@@ -40,59 +44,30 @@ public class Main {
             while (it.hasNext()) {
                 int prec = e;
                 e = it.next();
-                if (e % 2 == prec % 2) {
-                    ++lgBloc;
-                }
-                else {
+                if (e % 2 != prec % 2) {
                     lgBloc = 1;
-                }
-                if (lgBloc == 1) {
                     ++res;
                 }
-                else if (lgBloc == (LG + 1)){
-                    --res;
-                }
-            }
-        }
-        return res;
-    }
-    
-/*
-    Il est à noter que la fonction "nbBlocsMemePariteLongueurInfEgalLG" peut être re-factorisée comme ceci :
-
-    public static int nbBlocsMemePariteLongueurInfEgalLG2(SeqInt s, int LG) {
-        SeqIntIterator it = s.iterator();
-        int res = 0;
-        if (it.hasNext()) {
-            int e = it.next();
-            int lgBloc = 1;
-            res = 1;
-            while (it.hasNext()) {
-                int prec = e;
-                e = it.next();
-                if (e % 2 == prec % 2) {
+                else {
                     ++lgBloc;
-                    if (lgBloc == (LG +1)) {
+                    if (lgBloc == LG + 1) {
                         --res;
                     }
                 }
-                else {
-                    lgBloc = 1;
-                    ++res;
-                }
             }
         }
-        return res;
+        return  res;
     }
-*/
 
     public static void main(String[] args) {
-        SeqInt s = new SeqInt(2, 4, 1, 3, 1, 8, 6, 8, 8, 2, 4, 1, 7, 8, 8, 8, 1, 2);
-        System.out.println(s);
-        System.out.println("nbBlocsMemePariteLongueurInfEgalLG (lg=3) : 6 = " + nbBlocsMemePariteLongueurInfEgalLG(s, 3));
-        System.out.println("nbBlocsMemePariteLongueurInfEgalLG (lg=2) : 4 = " + nbBlocsMemePariteLongueurInfEgalLG(s, 2));
-        System.out.println("nbBlocsMemePariteLongueurInfEgalLG (lg=1) : 2 = " + nbBlocsMemePariteLongueurInfEgalLG(s, 1));
+        SeqInt s1 = new SeqInt();
+        SeqInt s2 = new SeqInt(1);
+        SeqInt s3 = new SeqInt(1, 1, 3, 4, 4, 8, 6, 7, 7, 1, 4, 1, 4, 1, 1, 8, 6, 6);
+        System.out.println("0 = " + nbBlocsMemePariteLongueurInfEgalLG(s1, 42));
+        System.out.println("1 = " + nbBlocsMemePariteLongueurInfEgalLG(s2, 1));
+        System.out.println("1 = " + nbBlocsMemePariteLongueurInfEgalLG(s2, 2));
+        System.out.println("3 = " + nbBlocsMemePariteLongueurInfEgalLG(s3, 1));
+        System.out.println("4 = " + nbBlocsMemePariteLongueurInfEgalLG(s3, 2));
+        System.out.println("7 = " + nbBlocsMemePariteLongueurInfEgalLG(s3, 3));
     }
-
-
 }
