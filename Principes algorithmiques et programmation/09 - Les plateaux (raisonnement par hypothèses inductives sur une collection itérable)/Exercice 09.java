1ae5b3cb5bafff7880e04ce9b2a28725d8341de2
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-02-18 16:47:25 +0100

    Update Exercice 09.java

--- Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 09.java
+++ Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 09.java
@@ -6,56 +6,70 @@ import seqint.SeqIntIterator;
 
 e = élément courant
 prec = élément précédent
-lgPlat = longueur du plateau courant
-res = nombre de plateaux de longueur égale à 1
+lgPlatCour = longueur du plateau courant
+res = nombre de plateaux de longueur 1
 
-prec              1 2 1 2 3 3 3 6 6 4 8 8 5 2 2     =>  prec = e
-e               1 2 1 2 3 3 3 6 6 4 8 8 5 2 2 1     => if (it.hasNext()) e = it.next()
-lgPlat          1 1 1 1 1 2 3 1 2 1 1 2 1 1 2 1     => if (e != prec) lgPlat = 1 else ++lgPlat
-LG[01]          1 2 3 4 5 4 4 5 4 5 6 5 6 7 6 7     => if (lgPlat == LG) ++res else if (lgPlat == (LG + 1)) --res
+prec              1 1 2 3 4 4 5 5 5 8 4 4 5 5   =>  prec = e
+e               1 1 2 3 4 4 5 5 5 8 4 4 5 5 4   =>  e = it.next()
+lgPlatCour      1 2 1 1 1 2 1 2 3 1 1 2 1 2 1   =>  if (e != prec) lgPlatCour = 1 else ++lgPlatCour
+res             1 0 1 2 3 2 3 2 2 3 4 3 4 3 4   =>   if (lgPlatCour == 1) ++res else if (lgPlatCour == 2) --res
 
 Pré-Initialisation :
-    int res = 0
+    int res = 0   => retourne zéro si la séquence est vide ou si il n'y a pas de plateaux de longueur 1
 
 Initialisation :
-    int e = it.next()
-    int lgPlat = 1
-    while (it.hasNext())
-    int prec = e
-    e = it.next()
- */
+    if (it.hasNext())
+        int e = it.next()
+        int lgPlatCour = 1
+        res = 1
+        while (it.hasNext())
+            int prec = e
+            e = it.next() ...
+*/
 
 public class Main {
 
-    public static int nombrePlatLong1(SeqInt s, int LG) {
+    public static int nbPlatLong1(SeqInt s) {
         SeqIntIterator it = s.iterator();
         int res = 0;
         if (it.hasNext()) {
             int e = it.next();
-            int lgPlat = 1;
+            int lgPlatCour = 1;
             res = 1;
             while (it.hasNext()) {
                 int prec = e;
                 e = it.next();
                 if (e != prec) {
-                    lgPlat = 1;
-                    ++res;
+                    lgPlatCour = 1;
                 }
                 else {
-                    ++lgPlat;
-                    if (lgPlat == (LG + 1)) {
-                        --res;
-                    }
+                    ++lgPlatCour;
+                }
+                if (lgPlatCour == 1) {
+                    ++res;
+                }
+                else if (lgPlatCour == 2) {
+                    --res;
                 }
             }
         }
-        return  res;
+        return res;
     }
 
     public static void main(String[] args) {
-        SeqInt s1 = new SeqInt();
-        System.out.println("0 = " + nombrePlatLong1(s1, 1));
-        SeqInt s2 = new SeqInt(1, 2, 1, 2, 3, 3, 3, 6, 6, 4, 8, 8, 5, 2, 2, 1);
-        System.out.println("7 = " + nombrePlatLong1(s2, 1));
+        SeqInt sVide = new SeqInt();
+        System.out.println(sVide);
+        System.out.println("nbPlateauxLong1 : 0 = " + nbPlatLong1(sVide));
+        System.out.println();
+
+        SeqInt s1 = new SeqInt(5, 2, 1, 1, 1, 2, 2, 7, 7, 7, 7, 1, 2);
+        System.out.println(s1);
+        System.out.println("nbPlateauxLong1 : 4 = " + nbPlatLong1(s1));
+        System.out.println();
+
+        SeqInt s2 = new SeqInt(5, 2, 1, 1, 2, 2, 5, 3, 3, 7, 1, 1, 7);
+        System.out.println(s2);
+        System.out.println("nbPlateauxLong1 : 5 = " + nbPlatLong1(s2));
+        System.out.println();
     }
 }
