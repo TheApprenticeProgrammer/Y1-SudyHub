1c28a6fb4b9390d6f1aec0e7cd1ce79c8127862b
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-03-02 17:04:52 +0100

    Update Exercice 01.java

--- Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 01.java
+++ Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 01.java
@@ -1,1 +1,22 @@
+/*
+Ecrivez une fonction qui détermine si un caractère est alphabétique minuscule. Ne vous occupez
+pas des diacritiques (accents).
+*/
 
+public class Main {
+
+    public static boolean estMin(char a) {
+        return ((int) a >= (int) 'a' && (int) a <= (int) 'z');
+    }
+
+    public static void main(String[] args) {
+        char c1 = 'a';
+        char c2 = 'z';
+        char c3 = '4';
+        char c4 = 'M';
+        System.out.println("true = " + estMin(c1));
+        System.out.println("true = " + estMin(c2));
+        System.out.println("false = " + estMin(c3));
+        System.out.println("false = " + estMin(c4));
+    }
+}
