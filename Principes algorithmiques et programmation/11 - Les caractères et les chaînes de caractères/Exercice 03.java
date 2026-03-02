374683182dab6d5c1666f5b2c9823b5dfb9cb4e3
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-03-02 17:35:21 +0100

    Create Exercice 03.java

---
+++ Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 03.java
@@ -0,0 +1,32 @@
+/*
+Ecrivez une fonction qui détermine si un string est formé exclusivement de minuscules. Utilisez
+votre fonction de la question 1.
+*/
+
+public class Main {
+
+    public static boolean estMin(char a) {
+        return ((int) a >= (int) 'a' && (int) a <= (int) 'z');
+    }
+
+    public static boolean stringFullMin(String s) {
+        boolean onlyMin = true;
+        for (int i = 0; i < s.length() && onlyMin; ++i) {
+            onlyMin = estMin(s.charAt(i));
+        }
+        return onlyMin;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "hello";
+        String s2 = "Hello";
+        String s3 = "hEllo";
+        String s4 = "heLLo";
+        String s5 = "hellO";
+        System.out.println("true = " + stringFullMin(s1));
+        System.out.println("false = " + stringFullMin(s2));
+        System.out.println("false = " + stringFullMin(s3));
+        System.out.println("false = " + stringFullMin(s4));
+        System.out.println("false = " + stringFullMin(s5));
+    }
+}
