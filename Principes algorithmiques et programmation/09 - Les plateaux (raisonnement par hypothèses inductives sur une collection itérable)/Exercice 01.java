1693e1d0a53e24ea0592e2444b4a4e7c66bd4ee8
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-03-08 12:18:45 +0100

    Update Exercice 01.java

--- Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 01.java
+++ Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 01.java
@@ -6,51 +6,55 @@ import seqint.SeqIntIterator;
 
 e = élément courant
 prec = élément précédent
-nbPlat = nombre de plateaux de la séquence
+res = nombre de plateaux de la séquence
 
-prec          1 1 2 3 3 4 5 5 5 5 8     =>  prec = e
-e           1 1 2 3 3 4 5 5 5 5 8 8     =>  e = it.next()
-nbPlat      1 1 2 3 3 4 5 5 5 5 6 6     =>  if (e != prec) ++nbPlat
+prec              1 1 2 2 2 4 5 8 8 8 4 4 4 4 4 1   =>  int prec = e;
+e               1 1 2 2 2 4 5 8 8 8 4 4 4 4 4 1 2   =>  e = it.next();
+res           0 1 1 2 2 2 3 4 5 5 5 6 6 6 6 6 7 8   =>  if (e != prec) ++res;
 
 Pré-Initialisation :
-    int nbPlat = 0     => renvoi zéro si la séquence est vide
+    SeqIntIterator it =s.iterator();
+    int res = 0;
+    return res;
 
 Initialisation :
     if (it.hasNext())
-        int e = it.next()
-        nbPlat = 1     => il y a au moins un plateau
+        int e = it.next();
+        res = 1;
         while (it.hasNext())
-            int prec = e
-            e = it.next() ...
+            int prec = e;
+            e = it.next();
+                ...
 */
 
 public class Main {
 
-    public static int nombrePlateaux(SeqInt s) {
-        SeqIntIterator it = s.iterator();
-        int nbPlat = 0;
+    public static int nbPlatSeq(SeqInt s) {
+        SeqIntIterator it =s.iterator();
+        int res = 0; // renvoie zéro si la séquence est vide
         if (it.hasNext()) {
             int e = it.next();
-            nbPlat = 1;
+            res = 1; // il y a au moins un élément donc un plateau dans la séquence
             while (it.hasNext()) {
                 int prec = e;
                 e = it.next();
                 if (e != prec) {
-                    ++nbPlat;
+                    ++res;
                 }
             }
         }
-        return nbPlat;
+        return res;
     }
 
     public static void main(String[] args) {
-        SeqInt s1 = new SeqInt();
-        SeqInt s2 = new SeqInt(1);
-        SeqInt s3 = new SeqInt(5, 5, 5, 5, 5);
-        SeqInt s4 = new SeqInt(1, 1, 4, 5, 4, 4, 8, 8, 8, 7, 3, 1, 1);
-        System.out.println("0 = " + nombrePlateaux(s1));
-        System.out.println("1 = " + nombrePlateaux(s2));
-        System.out.println("1 = " + nombrePlateaux(s3));
-        System.out.println("8 = " + nombrePlateaux(s4));
+        SeqInt sVide = new SeqInt();
+        System.out.println(sVide);
+        System.out.println("nbPlateaux : 0 = " + nbPlatSeq(sVide));
+        System.out.println();
+
+        SeqInt s = new SeqInt(5, 2, 1, 1, 1, 2, 2, 7, 7, 7, 7, 1, 2);
+        System.out.println(s);
+        System.out.println("nbPlateaux : 7 = " + nbPlatSeq(s));
+        System.out.println();
     }
 }
