d1f70cc7a0a625b076f919c2312d059108993e9a
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-03-27 19:35:11 +0100

    Update Exercice 07.java

--- Principes algorithmiques et programmation/10 - Les tableaux/Exercice 07.java
+++ Principes algorithmiques et programmation/10 - Les tableaux/Exercice 07.java
@@ -1,50 +1,63 @@
 import eu.epfc.prm.Array;
+import java.util.Scanner;
 
 /*
 Ecrivez une fonction qui renverse l’ordre des éléments d’un tableau. Si le tableau contient
 initialement les entiers [4, 7, 5, 2, 4, 3, 2, 3] ; il contiendra après renversement : [3,
 2, 3, 4, 2, 5, 7, 4].
-
-tab             4 7 5 2 4 3 2 3
-boucle[01]      3 7 5 2 4 3 2 4     =>  int save = tab.get(i) tab.set(tab.size - 1, save)
-boucle[02]      3 2 5 2 4 3 7 4     =>  int save = tab.get(i) tab.set(tab.size - 2, save)
-boucle[03]      3 2 3 2 4 5 7 4     =>  int save = tab.get(i) tab.set(tab.size - 3, save)
-boucle[04]      3 2 3 4 2 5 7 4     =>  int save = tab.get(i) tab.set(tab.size - 4, save)
-res             3 2 3 4 2 5 7 4
-
-Pré-Initialisation :
-    int size = tab.size -1
-
-Initialisation :
-    for (int i = 0; i < (tab.size / 2); ++i)    => si nb pair 8 / 2 = 4 | si nb impair 9 / 2 = 4 ok car le nombre du centre ne doit
-                                                                                                    pas être changé.
 */
 
 public class Main {
 
-    public static Array<Integer> reverseArray(Array<Integer> tab) {
-        int size = tab.size() - 1;
-        for (int i = 0; i < (tab.size() / 2); ++i) {
-            int save = tab.get(i);
-            tab.set(i, tab.get(size));
-            tab.set(size, save);
-            --size;
+    public static void switchWithWhile(Array<Integer> tab) {
+        int g = 0;
+        int d = tab.size() -1;
+        while (g < tab.size() / 2) {
+            int save = tab.get(g);
+            tab.set(g, tab.get(d));
+            tab.set(d, save);
+            ++g;
+            --d;
         }
-        return tab;
+    }
+
+    public static void switchWithFor(Array<Integer> tab) {
+        int d = tab.size() - 1;
+        for (int i = 0; i < tab.size() / 2; ++i) {
+            int save = tab.get(i);
+            tab.set(i, tab.get(d));
+            tab.set(d, save);
+            --d;
+        }
+    }
+
+    public static void echange(Array<Integer> tab, int id1, int id2) {
+        int tmp = tab.get(id1);
+        tab.set(id1, tab.get(id2));
+        tab.set(id2, tmp);
+    }
+
+    public static void switchWithEchange(Array<Integer> tab) {
+        int g = 0;
+        int d = tab.size() - 1;
+        while (g < d) {
+            echange(tab, g, d);
+            ++g;
+            --d;
+        }
     }
 
     public static void main(String[] args) {
-        Array<Integer> tab1 = new Array<>(4, 7, 5, 2, 4, 3, 2, 3);
-        System.out.println(tab1);
-        System.out.println();
-        reverseArray(tab1);
-        System.out.println(tab1);
-        System.out.println();
-        Array<Integer> tab2 = new Array<>(1, 5, 4, 3, 8, 2, 0, 7, 3, 6, 5);
-        System.out.println(tab2);
-        System.out.println();
-        reverseArray(tab2);
-        System.out.println(tab2);
+        Array<Integer> tab = new Array<>(4, 7, 5, 2, 4, 3, 2, 3);
+        System.out.println(tab);
+        System.out.println("Switch 01 : ");
+        switchWithWhile(tab);
+        System.out.println(tab);
+        System.out.println("Switch 02 : ");
+        switchWithFor(tab);
+        System.out.println(tab);
+        System.out.println("Switch 03 : ");
+        switchWithEchange(tab);
+        System.out.println(tab);
     }
 }
-
