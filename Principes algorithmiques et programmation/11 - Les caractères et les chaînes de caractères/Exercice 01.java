4e2658acb547ba9f457be543add05f825a9e0e3f
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-03-30 22:17:47 +0200

    Update Exercice 01.java

--- Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 01.java
+++ Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 01.java
@@ -1,22 +1,32 @@
 /*
-Ecrivez une fonction qui détermine si un caractère est alphabétique minuscule. Ne vous occupez
+Écrivez une fonction qui détermine si un caractère est alphabétique minuscule. Ne vous occupez
 pas des diacritiques (accents).
+
+Autre version :
+    public static boolean estMin(char c) {
+        return c >= 'a' && c <= 'z';
+    }
 */
 
 public class Main {
 
-    public static boolean estMin(char a) {
-        return ((int) a >= (int) 'a' && (int) a <= (int) 'z');
+    public static boolean estMin(char c) {
+        return (int) c >= (int) 'a' && (int) c <= (int) 'z';
     }
 
     public static void main(String[] args) {
-        char c1 = 'a';
-        char c2 = 'z';
-        char c3 = '4';
-        char c4 = 'M';
-        System.out.println("true = " + estMin(c1));
-        System.out.println("true = " + estMin(c2));
-        System.out.println("false = " + estMin(c3));
-        System.out.println("false = " + estMin(c4));
+        char c = 'b';
+        if (estMin(c)) {
+            System.out.println("Le caractère '" + c + "' est une minuscule");
+        } else {
+            System.out.println("Le caractère '" + c + "' n'est pas une minuscule");
+        }
+
+        c = 'Z';
+        if (estMin(c)) {
+            System.out.println("Le caractère '" + c + "' est une minuscule");
+        } else {
+            System.out.println("Le caractère '" + c + "' n'est pas une minuscule");
+        }
     }
 }
