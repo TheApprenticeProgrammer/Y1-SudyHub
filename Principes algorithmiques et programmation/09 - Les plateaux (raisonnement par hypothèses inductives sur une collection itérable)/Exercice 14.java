5ed38d301b11e0b724df093ce1d3761f4516d128
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-03-31 14:11:57 +0200

    Update Exercice 14.java

--- Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 14.java
+++ Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 14.java
@@ -5,48 +5,51 @@ import seqint.SeqIntIterator;
 Écrire une fonction qui calcule le nombre de plateaux de hauteur maximale et de longueur
 strictement supérieure à 1.
 
-prec              1 1 2 2 2 5 5 3 5 3 5 5 8 8 8 4 4 8 2 8   => int prec = e
-e               1 1 2 2 2 5 5 3 5 3 5 5 8 8 8 4 4 8 2 8 8   => if (it.hasNext()) e = it.next()
-lgPlat          1 2 1 2 3 1 2 1 1 1 1 2 1 2 3 1 2 1 1 1 2   => if (e == prec) ++lgPlat else lgPlat = 1
-max             1 1 2 2 2 5 5 5 5 5 5 5 8 8 8 8 8 8 8 8 8   => if (e > max) max = e res = 0
-res             0 1 0 1 1 0 1 1 1 1 1 2 0 1 1 1 1 1 1 1 2   => if (lgPlat == 2 && e == max) ++res
+prec              1 1 5 5 4 4 4 5 8 8 4 8 5 8 8     => int prec = e;
+e               1 1 5 5 4 4 4 5 8 8 4 8 5 8 8 8     => int e = it.next();
+lgPlatCour      1 2 1 2 1 2 3 1 1 2 1 1 1 1 2 3     => if (e != prec) lgPlatCour = 1; else ++lgPlatCour;
+max             1 1 5 5 5 5 5 5 8 8 8 8 8 8 8 8     => if (e > max) max = e; res = 0;
+res           0 0 1 0 1 1 1 1 1 0 1 1 1 1 1 2 2     => if (e == max && lgPlatCour == 2) ++res;
 
 Pré-Initialisation :
-    int res = 0
+    SeqIntIterator it = s.iterator();
+    int res = 0;
+    return res;
 
 Initialisation :
     if (it.hasNext())
-    int e = it.next()
-    int lgPlat = 1
-    int max = e;
-    while (it.hasnext())
-    int prec = e
-    e = it.next()
+        int e = it.next();
+        int max = e;
+        int lgPlatCour = 1;
+        while (it.hasNext())
+            int prec = e;
+            e = it.next();
+                ...
 */
 
 public class Main {
 
-    public static int nombrePlatHautMaxLongSup1(SeqInt s) {
+    public static int nbPlateauxHautMaxLongPlusGrand1(SeqInt s) {
         SeqIntIterator it = s.iterator();
         int res = 0;
         if (it.hasNext()) {
             int e = it.next();
-            int lgPlat = 1;
             int max = e;
-            while(it.hasNext()) {
+            int lgPlatCour = 1;
+            while (it.hasNext()) {
                 int prec = e;
                 e = it.next();
-                if (e == prec) {
-                    ++lgPlat;
-                    if (lgPlat == 2 && e == max) {
-                        ++res;
-                    }
-                }
-                else {
-                    lgPlat = 1;
+                if (e != prec) {
+                    lgPlatCour = 1;
                     if (e > max) {
                         max = e;
                         res = 0;
+                    }
+                }
+                else {
+                    ++lgPlatCour;
+                    if (e == max && lgPlatCour == 2) {
+                        ++res;
                     }
                 }
             }
@@ -55,9 +58,13 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        SeqInt s1 = new SeqInt();
-        System.out.println("0 = " + nombrePlatHautMaxLongSup1(s1));
-        SeqInt s2 = new SeqInt(1, 1, 2, 2, 2, 5, 5, 3, 5, 3, 5, 5, 8, 8, 8, 4, 4, 8, 2, 8, 8);
-        System.out.println("2 = " + nombrePlatHautMaxLongSup1(s2));
+        SeqInt s1 = new SeqInt(1, 1, 3, 3, 3, 2, 3, 1, 3, 3, 3, 5, 1, 1, 5, 5, 5, 4, 5, 5, 5, 9);
+        System.out.println("0 = " + nbPlateauxHautMaxLongPlusGrand1(s1));
+        SeqInt s2 = new SeqInt(1, 1, 3, 3, 3, 2, 3, 1, 3, 3, 3, 5, 1, 1, 5, 5, 5, 4, 5, 5, 5);
+        System.out.println("2 = " + nbPlateauxHautMaxLongPlusGrand1(s2));
+        SeqInt s3 = new SeqInt(1, 1, 3, 3, 3, 2, 3, 1, 3, 3, 3);
+        System.out.println("2 = " + nbPlateauxHautMaxLongPlusGrand1(s3));
+        SeqInt s4 = new SeqInt();
+        System.out.println("0 = " + nbPlateauxHautMaxLongPlusGrand1(s4));
     }
 }
