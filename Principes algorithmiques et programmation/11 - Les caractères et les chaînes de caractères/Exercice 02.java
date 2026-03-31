10d86bef2121239568b45713398acebdde21750b
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-03-31 15:43:39 +0200

    Update Exercice 02.java

--- Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 02.java
+++ Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 02.java
@@ -1,31 +1,22 @@
 /*
-Ecrivez une fonction qui renvoie la majuscule d’un caractère donné. Que renvoyez-vous si le
+Écrivez une fonction qui renvoie la majuscule d’un caractère donné. Que renvoyez-vous si le
 caractère n’est pas une minuscule ?
-
-Si minuscule    => if ((int) c >= (int) 'a' && (int) c <= (int) 'z')
-Si majuscule    => rien ...
-Si autre        => else if ((int) c < (int) 'A' || (int) c > (int) 'Z')
 */
 
 public class Main {
 
     public static char toMaj(char c) {
-        int nbToMaj = (int) 'a' - (int) 'A';
-        if ((int) c >= (int) 'a' && (int) c <= (int) 'z') {
-            c = (char) ((int) c - nbToMaj);
-        }
-        else if ((int) c < (int) 'A' || (int) c > (int) 'Z') {
-            throw new RuntimeException("Wrong character !");
+        if (c >= 'a' && c <= 'z') {
+            c = (char) ((int) c - ((int) 'a' - (int) 'A'));
         }
         return c;
     }
 
     public static void main(String[] args) {
-        char c1 = 'a';
-        char c2 = 'Z';
-        char c3 = '5';
-        System.out.println("A = " + toMaj(c1));
-        System.out.println("Z = " + toMaj(c2));
-        System.out.println(toMaj(c3));
+        char c = 'b';
+        System.out.println("La majuscule (si c'est une minuscule) de " + c + " est " + toMaj(c));
+
+        c = 'Z';
+        System.out.println("La majuscule (si c'est une minuscule) de " + c + " est " + toMaj(c));
     }
 }
