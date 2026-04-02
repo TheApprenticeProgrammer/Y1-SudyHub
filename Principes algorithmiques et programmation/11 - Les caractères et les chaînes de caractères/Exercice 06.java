739b5fa1dba3a524c83541f9acd07af120253f1f
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-04-02 20:05:22 +0200

    Update Exercice 06.java

--- Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 06.java
+++ Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 06.java
@@ -1,24 +1,22 @@
 /*
-Ecrivez une fonction qui renvoie une copie renversée d’un string. Par exemple le string "hello"
+Écrivez une fonction qui renvoie une copie renversée d’un string. Par exemple le string "hello"
 donne "olleh".
 */
 
 public class Main {
 
-    public static String reverseString(String s) {
+    public static String reverse(String s) {
         String res = "";
-        for (int i = s.length() - 1; i > -1; --i) {
+        for (int i = s.length() - 1; i >= 0; --i) {
             res += s.charAt(i);
         }
         return res;
     }
 
     public static void main(String[] args) {
-        String s1 = "hello";
-        String s2 = "Toji Zenin";
-        System.out.println(s1);
-        System.out.println(reverseString(s1));
-        System.out.println(s2);
-        System.out.println(reverseString(s2));
+        String s1 = "Hello";
+        System.out.println(reverse(s1));
+        String s2 = "Hello World !";
+        System.out.println(reverse(s2));
     }
 }
