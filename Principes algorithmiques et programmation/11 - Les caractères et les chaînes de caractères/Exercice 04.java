61efbe3ba21d9da5602d1b2f1e9e7bb2a55b485f
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-04-02 19:42:42 +0200

    Update Exercice 04.java

--- Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 04.java
+++ Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 04.java
@@ -1,35 +1,32 @@
 /*
-Ecrivez une fonction qui renvoie une copie en majuscule d’un string donné.
+Écrivez une fonction qui renvoie une copie en majuscule d’un string donné.
 */
-
 
 public class Main {
 
+    public static boolean estMin(char c) {
+        return c >= 'a' && c <= 'z';
+    }
+
     public static char toMaj(char c) {
-        int nbToMaj = (int) 'a' - (int) 'A';
-        if ((int) c >= (int) 'a' && (int) c <= (int) 'z') {
-            c = (char) ((int) c - nbToMaj);
-        }
-        else if ((int) c < (int) 'A' || (int) c > (int) 'Z') {
-            throw new RuntimeException("Wrong character !");
+        if (estMin(c)) {
+            return (char) ((int)c + ((int)'A' - (int)'a'));
         }
         return c;
     }
 
-    public static String allStringToMaj(String s) {
+    public static String allMaj(String s) {
         String res = "";
         for (int i = 0; i < s.length(); ++i) {
-            res = res + toMaj(s.charAt(i));
+            res += toMaj(s.charAt(i));
         }
         return res;
     }
 
     public static void main(String[] args) {
-        String s1 = "hello";
-        String s2 = "AaAaAa";
-        String s3 = "QrtP8";
-        System.out.println("hello => " + allStringToMaj(s1));
-        System.out.println("AaAaAa => " + allStringToMaj(s2));
-        System.out.println(allStringToMaj(s3));
+        String s1 = "Hello";
+        System.out.println(allMaj(s1));
+        String s2 = "Hello World !";
+        System.out.println(allMaj(s2));
     }
 }
