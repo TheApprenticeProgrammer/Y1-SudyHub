e96e481322b54340c2f00b0aa0c81d27a8ae3d1a
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-04-02 19:50:56 +0200

    Update Exercice 05.java

--- Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 05.java
+++ Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 05.java
@@ -1,16 +1,16 @@
 /*
-Ecrivez une fonction qui compte le nombre d’apparitions d’un caractère donné dans un string.
+Écrivez une fonction qui compte le nombre d’apparitions d’un caractère donné dans un string.
 Par exemple, le caractère 'o' apparaît 2 fois dans le mot "Onomatopée" car nous distinguons
 minuscules et majuscules.
 */
 
 public class Main {
 
-    public static int nbAppChar(String s, char c) {
+    public static int nbApparitions(String s, char c) {
         int res = 0;
         for (int i = 0; i < s.length(); ++i) {
-            char carCourant = s.charAt(i);
-            if ((int) carCourant == (int) c) {
+            char x = s.charAt(i);
+            if ((int)c == (int)x) {
                 ++res;
             }
         }
@@ -19,10 +19,7 @@ public class Main {
 
     public static void main(String[] args) {
         String s1 = "Onomatopée";
-        String s2 = "11éé2é";
-        System.out.println("2 = " + nbAppChar(s1, 'o'));
-        System.out.println("1 = " + nbAppChar(s1, 'O'));
-        System.out.println("3 = " + nbAppChar(s2, 'é'));
-        System.out.println("2 = " + nbAppChar(s2, '1'));
+        char c = 'O';
+        System.out.println("Le caractère " + c + " apparait " + nbApparitions(s1, c) + " fois.");
     }
 }
