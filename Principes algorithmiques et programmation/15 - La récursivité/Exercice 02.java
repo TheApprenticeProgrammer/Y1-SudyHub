a9a5e6393af1d7743e362128e116c2736c9e4156
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-04-02 21:13:39 +0200

    Update Exercice 02.java

--- Principes algorithmiques et programmation/15 - La récursivité/Exercice 02.java
+++ Principes algorithmiques et programmation/15 - La récursivité/Exercice 02.java
@@ -1,36 +1,35 @@
-import java.util.Scanner;
 /*
-Ecrivez une fonction récursive qui renvoie la factorielle d'un nombre entier positif.
+Écrivez une fonction récursive qui renvoie la factorielle d'un nombre entier positif.
 
-if (5 > 1) => 5 * fact(4)
-                    => if (4 > 1) => 4 * fact(3)
-                                            => if (3 > 1) => 3 * fact(2)
-                                                                    => if (2 > 1) => 2 * fact(1)
-                                                                                            => if (1 > 1) => STOP => else => return 1
-Ce qui donne :
-    2 * 1 = 2
-    3 * 2 = 6
-    4 * 6 = 24
-    5 * 24 = 120
+Formule générale : n! = n * (n - 1) * ((n - 1) - 1) ... * 1
+Pour 5! = 5 * 4 * 3 * 2 * 1
+
+Cas de base => n == 0 => return 1
+
+return 5 * f(5 - 1)     <== 5 * 4 * 3 * 2 * 1
+                => 4 * f(4 - 1)     <== 4 * 3 * 2 * 1 * 1
+                            => 3 * f(3 - 1)     <== 3 * 2 * 1 * 1
+                                        => 2 * f(2 - 1)     <== 2 * 1 * 1
+                                                    => 1 * f(1 - 1)     <== 1 * 1
+                                                            => 0 => Cas de base return 1;
 */
 
 public class Main {
-    public static int fact(int val) {
-        if (val < 0) {
-            throw new RuntimeException("Nombre négatif !");
+
+    public static int factorielle(int n) {
+        if (n < 0) {
+            throw new RuntimeException("Impossible => wrong number !");
         }
-        if (val > 1) {
-            return val * fact(val - 1);
+        if (n == 0) {
+            return 1;
         }
         else {
-            return 1;
+            return n * factorielle(n - 1);
         }
     }
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Entrez un nombre : ");
-        int val = scan.nextInt();
-        System.out.println("La factorielle de " + val + " est : " + fact(val));
+        System.out.println("120 = " + factorielle(5));
+        System.out.println(factorielle(-1));
     }
 }
