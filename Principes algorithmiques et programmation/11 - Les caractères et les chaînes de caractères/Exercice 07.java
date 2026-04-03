8230bf00a6fa540d0407658ff014a2eda3a544b8
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-04-03 13:03:49 +0200

    Update Exercice 07.java

--- Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 07.java
+++ Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 07.java
@@ -1,5 +1,5 @@
 /*
-Ecrivez une fonction qui détermine si un string forme un palindrome (il est égal à sa copie
+Écrivez une fonction qui détermine si un string forme un palindrome (il est égal à sa copie
 renversée). Par exemple radar forme un palindrome. Est-ce nécessaire de le renverser pour le
 déterminer ?
 */
@@ -7,25 +7,32 @@ déterminer ?
 public class Main {
 
     public static boolean estPalindrome(String s) {
-        boolean estPal = true;
-        int otherSide = s.length() - 1;
-        for (int i = 0; i < s.length() / 2 && estPal; ++i) {
-            char min = s.charAt(i);
-            char max = s.charAt(otherSide);
-            if ((int) min != (int) max) {
-                estPal = false;
+        boolean palindrome = true;
+        int cpt = s.length() - 1;
+        for (int i = 0; i < s.length() / 2 && palindrome; ++i) {
+            char a = s.charAt(cpt - i);
+            if ((int) s.charAt(i) != (int) a) {
+                palindrome = false;
             }
-            --otherSide;
         }
-        return estPal;
+        return palindrome;
+    }
+
+    public static boolean estPalindromeV2(String s) {
+        int g = 0, d = s.length() - 1;
+        while (g < d && s.charAt(g) == s.charAt(d)) {
+            ++g;
+            --d;
+        }
+        return g >= d;
     }
 
     public static void main(String[] args) {
-        String s1 = "radar";
-        String s2 = "AroprA";
-        String s3 = "ressasser";
-        System.out.println("true = " + estPalindrome(s1));
-        System.out.println("false = " + estPalindrome(s2));
-        System.out.println("true = " + estPalindrome(s3));
+        String s1 = new String("radar");
+        System.out.println(s1 + " est un palindrome : " + estPalindrome(s1));
+        String s2 = new String("rador");
+        System.out.println(s2 + " est un palindrome : " + estPalindrome(s2));
+        String s3 = new String("ressasser");
+        System.out.println(s3 + " est un palindrome : " + estPalindrome(s3));
     }
 }
