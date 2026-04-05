f94604523fb61f1d500c31d07ffb1c2975701ec3
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-04-05 17:58:07 +0200

    Update Exercice 09.java

--- Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 09.java
+++ Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 09.java
@@ -1,35 +1,37 @@
 /*
-Ecrivez une fonction qui renvoie l’indice de la première apparition d’un caractère dans un
+Écrivez une fonction qui renvoie l’indice de la première apparition d’un caractère dans un
 string. Que renvoyez-vous si le caractère n’apparaît pas dans le string ?
+
+Version du corrigé :
+    public static int pos(char c, String s) {
+        int k = 0;
+        while (k < s.length() && s.charAt(k) != c) {
+            ++k;
+        }
+        return k;
+    }
 */
 
 public class Main {
 
-    public static int firstPosCar(String s, char c) {
-        int res = 0;
-        if (s.length() > 0) {
-            int posCour = 1;
-            boolean find = false;
-            for (int i = 0; i < s.length() && !find; ++i) {
-                int car = s.charAt(i);
-                if (car == c) {
-                    res = posCour;
-                    find = true;
-                }
-                ++posCour;
+    public static int indicePremApp(String s, char c) {
+        int res = - 1;
+        boolean find = false;
+        for (int i = 0; i < s.length() && !find; ++i) {
+            if ((int) s.charAt(i) == (int) c) {
+                res = i;
+                find = true;
             }
         }
         return res;
     }
 
     public static void main(String[] args) {
-        String s1 = "";
-        String s2 = "azeAtyuiop";
-        String s3 = "Ahhhhhhhha";
-        String s4 = "Ahhhhahhhhha";
-        System.out.println("0 = " + firstPosCar(s1, 'a'));
-        System.out.println("4 = " + firstPosCar(s2, 'A'));
-        System.out.println("10 = " + firstPosCar(s3, 'a'));
-        System.out.println("1 = " + firstPosCar(s4, 'A'));
+        String s1 = "Hello World !";
+        System.out.println("2 = " + indicePremApp(s1, 'l'));
+        System.out.println("6 = " + indicePremApp(s1, 'W'));
+        System.out.println("5 = " + indicePremApp(s1, ' '));
+        System.out.println("12 = " + indicePremApp(s1, '!'));
+        System.out.println("-1 = " + indicePremApp(s1, 'y'));
     }
 }
