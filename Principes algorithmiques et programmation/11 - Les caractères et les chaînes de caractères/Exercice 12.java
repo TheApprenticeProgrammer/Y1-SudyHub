a562b119f0fb9f4b1690f5a5e8358e49fcdcafde
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-04-08 10:52:25 +0200

    Update Exercice 12.java

--- Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 12.java
+++ Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 12.java
@@ -1,70 +1,45 @@
-/*
-Comme vu dans les slides, en Java, les opérateurs de comparaison (<, >, …) ne sont pas définis
-sur les chaînes de caractères (mais bien sur les caractères eux-mêmes). Ecrivez, sans utiliser la
-méthode compareTo(), la fonction int compare(String ch1, String ch2) qui renvoie
-un résultat négatif (quelconque) si ch1 < ch2, nul si ch1 == ch2 et positif si ch1 > ch2.
-*/
-
 public class Main {
 
     public static int compare(String ch1, String ch2) {
-        int res = 0;
-        int lg = 0;
-        if (ch1.length() >= ch2.length()) {
-            lg = ch2.length();
+        int lg = Math.min(ch1.length(), ch2.length());
+        int cnt = 0;
+        // On avance tant que les caractères sont identiques
+        while (cnt < lg && ch1.charAt(cnt) == ch2.charAt(cnt)) {
+            cnt++;
         }
-        else {
-            lg = ch1.length();
+        if (cnt < lg) {
+            // Une première différence a été trouvée
+            return ch1.charAt(cnt) - ch2.charAt(cnt);
+        } else {
+            // Aucun caractère différent sur la longueur minimale
+            return ch1.length() - ch2.length();
         }
-        boolean end = false;
-        for (int i = 0; i < lg && !end; ++i) {
-            char car1 = ch1.charAt(i);
-            char car2 = ch2.charAt(i);
-            if ((int) car1 != (int) car2) {
-                if ((int) car1 > (int) car2) {
-                    res = 1;
-                    end = true;
-                }
-                else {
-                    res = -1;
-                    end = true;
-                }
+    }
+
+    public static int compareV2(String ch1, String ch2) {
+        int lg = Math.min(ch1.length(), ch2.length());
+        for (int i = 0; i < lg; i++) {
+            if (ch1.charAt(i) != ch2.charAt(i)) {
+                return ch1.charAt(i) - ch2.charAt(i);
             }
         }
-        if (res == 0 && ch1.length() > ch2.length()) {
-            res = 1;
-        }
-        else if (res == 0 && ch2.length() > ch1.length()) {
-            res = -1;
-        }
-        return res;
+        return ch1.length() - ch2.length();
     }
 
     public static void main(String[] args) {
+        String s1 = "Hello World";
+        String s2 = "Hello Worlde";
+        String s3 = "Hello World";
+        String s4 = "Hello Worlde";
 
-        // Cas identiques
-        System.out.println("== Cas identiques ==");
-        System.out.println("0 -> " + compare("Hello", "Hello"));
+        System.out.println(compare(s1, s2));
+        System.out.println(compare(s2, s1));
+        System.out.println(compare(s1, s3));
+        System.out.println(compare(s2, s4));
 
-        // Cas simples (différence interne)
-        System.out.println("\n== Différence dans les caractères ==");
-        System.out.println("1 -> " + compare("Hello", "Hella"));  // 'o' > 'a'
-        System.out.println("-1 -> " + compare("Hella", "Hello")); // 'a' < 'o'
-
-        // Cas longueurs différentes mais mêmes débuts
-        System.out.println("\n== Longueurs différentes, mêmes préfixes ==");
-        System.out.println("-1 -> " + compare("abc", "abcd"));   // devrait être -1
-        System.out.println("1 -> " + compare("abcd", "abc"));    // devrait être 1
-
-        // Cas où ch2 est vide
-        System.out.println("\n== Cas avec chaîne vide ==");
-        System.out.println("1 -> " + compare("abc", ""));        // devrait être 1
-        System.out.println("-1 -> " + compare("", "abc"));       // devrait être -1
-        System.out.println("0 -> " + compare("", ""));           // égalité
-
-        // Cas complètement différents
-        System.out.println("\n== Cas différents ==");
-        System.out.println("-1 -> " + compare("Apple", "Banana"));
-        System.out.println("1 -> " + compare("Banana", "Apple"));
+        System.out.println(compareV2(s1, s2));
+        System.out.println(compareV2(s2, s1));
+        System.out.println(compareV2(s1, s3));
+        System.out.println(compareV2(s2, s4));
     }
 }
