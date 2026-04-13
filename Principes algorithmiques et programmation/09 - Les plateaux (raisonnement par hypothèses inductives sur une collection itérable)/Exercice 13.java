b42304e6df2f1bc602bb8fb8913b4a70a462d86e
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-04-13 19:02:43 +0200

    Update Exercice 13.java

--- Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 13.java
+++ Principes algorithmiques et programmation/09 - Les plateaux (raisonnement par hypothèses inductives sur une collection itérable)/Exercice 13.java
@@ -6,24 +6,27 @@ import seqint.SeqIntIterator;
 
 e = élément courant
 prec = élément précédent
-max = valeur du/des plateau(x) de hauteur maximale
-res = nombre de plateaux de hauteur maximale
+newPlat = si e est le premier élément d'un nouveau plateau
+max = valeur du plateau de hauteur maximale
+res = nombre de plateau de hauteur maximale
 
-prec              1 1 5 4 4 5 5 8 8 8 2 2 8 8 5     =>  int prec = e;
-e               1 1 5 4 4 5 5 8 8 8 2 2 8 8 5 8     =>  int e = it.next();
-max             1 1 5 5 5 5 5 8 8 8 8 8 8 8 8 8     =>  if (e > max) max = e; res = 1;
-res           0 1 1 1 1 1 2 2 1 1 1 1 1 2 2 2 3     =>  if (e != prec && e == max) ++res;
+prec              4 4 2 1 2 4 5 5 4 1 1 5 5 2   =>  int prec = e;
+e               4 4 2 1 2 4 5 5 4 1 1 5 5 2 5   =>  int e = it.next();
+newPlat         t f t t t t t f t t f t f t t   =>  if (e != prec) newPlat = true;
+max             4 4 4 4 4 4 5 5 5 5 5 5 5 5 5   =>  if (e > max) max = e;
+res           0 1 1 1 1 1 2 1 1 1 1 1 2 2 2 3   =>  if (e == max && newPlat) ++res; newPlat = false;
 
 Pré-Initialisation :
-    SeqIntIterator it =s.iterator();
+    SeqIntIterator it = s.iterator();
     int res = 0;
     return res;
 
 Initialisation :
-    if (ithasNext())
+    if (it.hasNext())
         int e = it.next();
         int max = e;
-        int res = 1;
+        boolean newPlat = false;
+        res = 1;
         while (it.hasNext())
             int prec = e;
             e = it.next();
@@ -32,8 +35,35 @@ Initialisation :
 
 public class Main {
 
-    public static int nbAppMax(SeqInt s) {
-        SeqIntIterator it =s.iterator();
+    public static int nbPlatHautMax(SeqInt s) {
+        SeqIntIterator it = s.iterator();
+        int res = 0;
+        if (it.hasNext()) {
+            int e = it.next();
+            int max = e;
+            boolean newPlat = false;
+            res = 1;
+            while (it.hasNext()) {
+                int prec = e;
+                e = it.next();
+                if (e != prec) {
+                    newPlat = true;
+                    if (e > max) {
+                        max = e;
+                        res = 0;
+                    }
+                }
+                if (max == e && newPlat) {
+                    ++res;
+                    newPlat = false;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static int nbPlatHautMaxV2(SeqInt s) {
+        SeqIntIterator it = s.iterator();
         int res = 0;
         if (it.hasNext()) {
             int e = it.next();
@@ -44,9 +74,9 @@ public class Main {
                 e = it.next();
                 if (e > max) {
                     max = e;
-                    res = 1;
+                    res = 0;
                 }
-                else if (e != prec && e == max) {
+                if (max == e && e != prec) {
                     ++res;
                 }
             }
@@ -55,7 +85,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        SeqInt s = new SeqInt(1, 1, 5, 4, 4, 5, 5, 8, 8, 8, 2, 2, 8, 8, 5, 8);
-        System.out.println("3 = " + nbAppMax(s));
+        SeqInt s1 = new SeqInt();
+        System.out.println("0 = " + nbPlatHautMaxV2(s1));
+        SeqInt s2 = new SeqInt(1, 1, 2, 2, 1, 2, 2, 2, 4, 4, 2, 4, 4, 3, 4, 5);
+        System.out.println("1 = " + nbPlatHautMaxV2(s2));
+        SeqInt s3 = new SeqInt(1, 1, 2, 2, 1, 2, 2, 2, 4, 4, 2, 4, 4, 3, 4);
+        System.out.println("3 = " + nbPlatHautMaxV2(s3));
     }
 }
