49f11dd50bb170745c6eaece2039076665e68695
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-04-15 16:00:00 +0200

    Update Exercice 14.java

--- Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 14.java
+++ Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 14.java
@@ -1,39 +1,51 @@
 /*
-Écrivez une fonction intPositifToString() qui reçoit un entier positif ou nul et renvoie un string
+Ecrivez une fonction intPositifToString() qui reçoit un entier positif ou nul et renvoie un string
 contenant la représentation décimale de cet entier. Par exemple l’entier 123 donne le String (texte)
-"123". Votre fonction doit déclencher une exception si l’entier est négatif. Écrivez une autre
+"123". Votre fonction doit déclencher une exception si l’entier est négatif. Ecrivez une autre
 fonction intToString() qui peut recevoir tout entier positif ou négatif. Utilisez la première fonction.
-*/
 
-import seqint.SeqInt;
+Version du corrigé :
+public static String intPositifToString(int val) {
+        String res = "";
+        do {
+            int x = val % 10;
+            char c = (char)(x + (int)'0');
+            res = c + res;
+            val /= 10;
+        } while (val != 0);
+        return res;
+    }
+*/
 
 public class Main {
 
     public static String intPositifToString(int val) {
         String res = "";
-        if (val < 0) {
-            throw new RuntimeException("Negative number");
-        }
-        if (val == 0) {
-            res = "0";
-        }
         while (val != 0) {
             int valDiv = val % 10;
-            res = valDiv + res;
             val /= 10;
+            char c = (char) ((int)'0' + (int) valDiv);
+            res = c + res;
+        }
+        return res;
+    }
+
+    public static String intToString(int val) {
+        String res = intPositifToString(Math.abs(val));
+        if (val < 0) {
+            res = "-" + res;
         }
         return res;
     }
 
     public static void main(String[] args) {
-        int val1 = 123;
-        int val2 = 0;
-        int val3 = 987654321;
-        String s1 = intPositifToString(val1);
-        System.out.println(s1);
-        String s2 = intPositifToString(val2);
-        System.out.println(s2);
-        String s3 = intPositifToString(val3);
-        System.out.println(s3);
+        int val = 45720;
+        System.out.println("Ceci est un entier : " + val);
+        String sVal = intToString(val);
+        System.out.println("Ceci est un String : " + sVal);
+        val = -45720;
+        System.out.println("Ceci est un entier : " + val);
+        sVal = intToString(val);
+        System.out.println("Ceci est un String : " + sVal);
     }
 }
