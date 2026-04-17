f9931d5999cd8c798c05cae0e187bcf3092d7329
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-04-17 15:43:08 +0200

    Update Exercice 01.java

--- Principes algorithmiques et programmation/Les classes, les objets et les attributs/Exercice 01.java
+++ Principes algorithmiques et programmation/Les classes, les objets et les attributs/Exercice 01.java
@@ -1,88 +1,69 @@
+import eu.epfc.prm.Array;
 import java.util.Scanner;
-
-/*
-Exercice 01 :
-Écrivez un programme qui demande à l’utilisateur de faire la saisie au clavier de deux Personnes,
-les réaffiche à l’écran, et détermine laquelle est la plus âgée et laquelle précède l’autre dans le
-bottin.
-Il est important de bien modulariser votre solution : utilisez des fonctions pour chaque opération
-identifiable. Écrivez donc, au minimum, les fonctions suivantes :
-*/
 
 public class Main {
 
-/*
-Partie 01 :
+    /*
+Question 1 :
+
+Partie 1 :
 public static void saisie(Date d) qui complète la Date donnée en demandant à
 l’utilisateur le jour, le mois et l’année. On supposera, sans le vérifier, que la date rentrée
 est valide.
 */
     public static void saisie(Date d) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Veuillez entrer le jour : ");
+        System.out.println("Création d'une nouvelle date :");
+        System.out.println("Entrer le jour : ");
         d.jour = scan.nextInt();
-        System.out.println();
-        System.out.print("Veuillez entrer le mois : ");
+        System.out.println("Entrer le mois : ");
         d.mois = scan.nextInt();
-        System.out.println();
-        System.out.print("Veuillez entrer l'année : ");
+        System.out.println("Entrer l'année : ");
         d.annee = scan.nextInt();
-        System.out.println();
     }
 
 /*
 Partie 2 :
 public static void saisie(Personne p) qui complète la Personne donnée en
 demandant à l’utilisateur son nom, son prénom et sa date de naissance. On supposera que
-le nom et le prénom d’une personne ne contiennent qu’un seul mot.
+le nom et le prénom d’une personne ne contienne qu’un seul mot.
 */
     public static void saisie(Personne p) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Veuillez entrer le nom : ");
+        System.out.println("Création d'une nouvelle personne : ");
+        System.out.println("Entrer le nom : ");
         p.nom = scan.nextLine();
-        System.out.println();
-        System.out.print("Veuillez entrer le prénom : ");
+        System.out.println("Entrer le prénom : ");
         p.prenom = scan.nextLine();
-        System.out.println();
         p.ddn = new Date();
         saisie(p.ddn);
     }
 
 /*
-Partie 03 :
-public static void affiche(Date d) qui affiche la Date donnée.
+Partie 3 :
+public static void affiche(Date d) qui affiche la Date donnée
 */
     public static void affiche(Date d) {
-        System.out.print(d.jour + "/" + d.mois + "/" + d.annee);
-        System.out.println();
+        System.out.println(d.jour + "/" + d.mois + "/" + d.annee);
     }
 
 /*
-Partie 04 :
+Partie 4 :
 public static void affiche(Personne p) qui affiche le nom, le prénom et la date de
 naissance de la Personne donnée.
 */
     public static void affiche(Personne p) {
         System.out.println("Nom : " + p.nom);
         System.out.println("Prénom : " + p.prenom);
-        System.out.print("Date de naissance : ");
         affiche(p.ddn);
-        System.out.println();
     }
 
 /*
-Partie 05 :
+Partie 5 :
 public static int compare(Date d1, Date d2) qui compare deux Dates. Celle-ci
 renverra un entier positif (quelconque) si d1 est plus « grand » que d2, négatif si d1 est
 plus « petit » que d2 et 0 si d1 et d2 sont égales. Que signifie qu’une Date est plus grande
 qu’une autre ?
-
-Autre version du corrigé :
-    public static int compare2(Date d1, Date d2) {
-        return (d1.annee * 10000 + d1.mois * 100 + d1.jour) - (d2.annee * 10000 + d2.mois * 100 + d2.jour);
-    }
-
-
 */
     public static int compare(Date d1, Date d2) {
         int cmp = Integer.compare(d1.annee, d2.annee);
@@ -95,18 +76,22 @@ Autre version du corrigé :
         return cmp;
     }
 
+    public static int compareV2(Date d1, Date d2) {
+        return (10000 * d1.annee + 100 * d1.mois + d1.jour) - (10000 * d2.annee + 100 * d2.mois + d2.jour);
+    }
+
 /*
-Partie 06 :
+Partie 6 :
 public static int compareAge(Personne p1, Personne p2) qui compare deux
 Personnes selon leur âge (en jours). Elle renverra un positif si p1 est plus agé que p2, un
 négatif si p2 est plus agé que p1 et 0 si les deux Personnes sont nées le même jour.
 */
     public static int compareAge(Personne p1, Personne p2) {
-        return -compare(p1.ddn, p2.ddn);
+        return -(compare(p1.ddn, p2.ddn));
     }
 
 /*
-Partie 07 :
+Partie 7 :
 public static int compareNomPrenom(Personne p1, Personne p2) qui compare
 deux Personnes selon l’ordre alphabétique de leur nom et de leur prénom.
 */
@@ -118,32 +103,61 @@ deux Personnes selon l’ordre alphabétique de leur nom et de leur prénom.
         return cmp;
     }
 
-    public static void main(String[] args) {
-        Personne p1 = new Personne();
-        saisie(p1);
-        Personne p2 = new Personne();
-        saisie(p2);
-        affiche(p1);
-        affiche(p2);
-        if (compareAge(p1, p2) > 0) {
-            System.out.println(p1.nom + " " + p1.prenom + " est plus vieux que " + p2.nom + " " + p2.prenom);
+/*
+Question 2 :
+Complétez votre programme afin qu’il demande à l’utilisateur d’entrer une série de n Personnes
+(la valeur de n'est indiquée au préalable par l’utilisateur), les range dans un tableau et les affiche.
+*/
+    public static void saisie(Array<Personne> tab) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Combien de nouvelles personnes voulez-vous créer ? :");
+        int nbrPersonne = scan.nextInt();
+        if (nbrPersonne < 1) {
+            throw new RuntimeException("Nombre invalide");
         }
-        else if (compareAge(p1, p2) < 0) {
-            System.out.println(p2.nom + " " + p2.prenom + " est plus vieux que " + p1.nom + " " + p1.prenom);
-        }
-        else {
-            System.out.println("Ils ont le même age !");
-        }
-        if (compareNomPrenom(p1, p2) > 0) {
-            System.out.println(p2.nom + " " + p2.prenom + " est avant " + p1.nom + " " + p1.prenom);
-        }
-        else if (compareNomPrenom(p1, p2) < 0) {
-            System.out.println(p1.nom + " " + p1.prenom + " est avant " + p2.nom + " " + p2.prenom);
-        }
-        else {
-            System.out.println("Les noms et prénoms des personnes sont les mêmes !");
+        for (int i = 0; i < nbrPersonne; ++i) {
+            Personne p = new Personne();
+            saisie(p);
+            tab.add(p);
         }
     }
+
+    public static void affiche(Array<Personne> tab) {
+        for (int i = 0; i < tab.size(); ++i) {
+            affiche(tab.get(i));
+        }
+    }
+
+    public static void question2() {
+        Array<Personne> tab = new Array<>();
+        saisie(tab);
+        affiche(tab);
+    }
+
+/*
+Question 03 :
+Complétez votre programme pour afficher la personne la plus jeune du tableau. Utilisez pour cela
+votre fonction compareAge.
+*/
+    public static void personneLaPlusJeuneDuTableau(Array<Personne> tab) {
+        int indexPersonneLaPlusJeune = 0;
+        for (int i = 1; i < tab.size(); ++i) {
+            if (compareAge(tab.get(indexPersonneLaPlusJeune), tab.get(i)) > 0) {
+                indexPersonneLaPlusJeune = i;
+            }
+        }
+        System.out.println("La personne la plus jeune du tableau est :");
+        affiche(tab.get(indexPersonneLaPlusJeune));
+    }
+
+    public static void question3() {
+        Array<Personne> tab = new Array<>();
+        saisie(tab);
+        personneLaPlusJeuneDuTableau(tab);
+    }
+
+    public static void main() {
+        question2();
+        question3();
+    }
 }
-
-
