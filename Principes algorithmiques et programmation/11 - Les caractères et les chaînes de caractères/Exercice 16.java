e9a0c24fe62f58bc3be0ab2f558fbf1accdc10f9
The Apprentice Programmer <theapprenticeprogrammer@gmail.com>
2026-05-12 09:30:37 +0200

    Update Exercice 16.java

--- Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 16.java
+++ Principes algorithmiques et programmation/11 - Les caractères et les chaînes de caractères/Exercice 16.java
@@ -1,43 +1,58 @@
+import eu.epfc.prm.Array;
 import java.util.Scanner;
 
 /*
-Ecrivez un programme qui affiche les fréquences d’apparitions des caractères alphabétiques d’un
+Écrivez un programme qui affiche les fréquences d’apparitions des caractères alphabétiques d’un
 texte entré par l’utilisateur. Les autres caractères sont ignorés. Majuscules et minuscules sont
 comptabilisées conjointement. Par exemple, le texte « La vie est belle » donnera l’affichage :
 A: 1, B: 1, E: 4, I: 1, L: 3, S: 1, T: 1, V: 1. Pour permettre à l’utilisateur
 d’entrer une phrase entière, utilisez un Scanner avec la méthode nextLine(). Utilisez un tableau
 (Array) pour mémoriser les nombres d’apparitions des différents caractères.
 */
+
 public class Main {
 
-    public static int nbAppChar(String s, int val) {
-        int cpt = 0;
-        for (int i = 0; i < s.length(); ++i) {
-            if ((int) s.charAt(i) == val || (val + ((int) 'a' - (int) 'A')) == (int) s.charAt(i)) {
-                ++cpt;
-            }
-        }
-        return cpt;
+    public static boolean estLettre(char c) {
+        return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
     }
 
-    public static void sout(String s) {
-        int val = (int) 'A';
-        for (int i = 0; i <= 26; ++i) {
-            if (nbAppChar(s, val) != 0) {
-                System.out.print((char) val + " : " + nbAppChar(s, val));
-                System.out.print(", ");
+    public static boolean estMin(char c) {
+        return c >= 'a' && c <= 'z';
+    }
+
+    public static char toMaj(char c) {
+        if (estMin(c)) {
+            c = (char) (c - ('a' - 'A'));
+        }
+        return c;
+    }
+
+    public static Array<Integer> nbApparition(String s) {
+        Array<Integer> tab = new Array<>(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (estLettre(c)) {
+                c = toMaj(c);
+                tab.set((int)c - 'A', tab.get((int)c - 'A') + 1);
             }
-            ++val;
+        }
+        return tab;
+    }
+
+    public static void affiche(String s) {
+        Array<Integer> tab = nbApparition(s);
+        for (int i = 0; i < tab.size(); ++i) {
+            if (tab.get(i) != 0) {
+                char lettre = (char) ((int)'A' + i);
+                System.out.print(lettre + ": " + tab.get(i) + ", ");
+            }
         }
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String s1 = new String("La vie est belle");
-        sout(s1);
-        System.out.println();
-        System.out.println("À vous :");
-        String phrase = scan.nextLine();
-        sout(phrase);
+        String s = scan.nextLine();
+        System.out.println(s);
+        affiche(s);
     }
 }
